6f062537fcd7fd49f18a37a4fe7094082480fafe
Bayashat <baiashat2002@gmail.com>
2024-04-10 22:14:16 +0500

    add: JWT token parse, Roles

--- app/src/dependencies.py
+++ app/src/dependencies.py
@@ -1,46 +1,13 @@
-# async def verify_jwt(credentials: HTTPAuthorizationCredentials) -> bool:
-#     token = str(credentials.credentials)
-#     try:
-#         response = await AsyncRequestClient.make_request(
-#             method="GET",
-#             url="https://api.technodom.kz/sso/api/v1/profile",
-#             headers={"Authorization": f"Bearer {token}"},
-#         )
-#         response.raise_for_status()
-#         return True
-#     except httpx.HTTPStatusError as e:
-#         raise HTTPException(
-#             detail=f"HTTP error occurred: {e}", status_code=e.response.status_code
-#         )
-#     except httpx.RequestError as e:
-#         raise HTTPException(detail=f"Request error occurred: {e}", status_code=500)
-
-
-
-
-# class JWTBearer(HTTPBearer):
-#     async def __call__(self, request: Request):
-#         credentials: HTTPAuthorizationCredentials | None = await super(
-#             JWTBearer, self
-#         ).__call__(request)
-#         if credentials and await verify_jwt(credentials):
-#             return credentials.credentials
-#         else:
-#             return None
-
 from jose import jwt
-from fastapi.security import OAuth2PasswordBearer as OA
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from app.src.database import SessionLocal
+from fastapi import  Depends, Request, HTTPException, status
 
-# JWT part
-def create_jwt(user_id: int) -> str:
-    body = {"user_id": user_id}
-    token = jwt.encode(body, "fox_paper", algorithm="HS256")
-    return token
-    
-def decode_jwt(token: str) -> int:
-    data = jwt.decode(token, "fox_paper", algorithms="HS256")
-    return data["user_id"]
+from app.src.models.user import User
+from .routers.repositories.users import UsersRepository
+from .routers.services.users import get_user_by_email
+
+from .config import ALGORITHM, SECRET_KEY
 
 # Database part
 def get_db():
@@ -50,4 +17,135 @@ def get_db():
     finally: 
         db.close()
         
-oauth2_scheme = OA(tokenUrl="/auth/users/login")
+
+class JWTRepo:
+    @staticmethod
+    def generate_token(data: dict):
+        return jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM)
+    
+    @staticmethod
+    def decode_token(token: str):
+        try:
+            return jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
+        except Exception as e:
+            print(f"Error decoding token: {e}")
+            return None
+
+
+class JWTBearer(HTTPBearer):
+    def __init__(self, auto_error: bool = True):
+        super(JWTBearer, self).__init__(auto_error=auto_error)
+
+    async def __call__(self, request: Request):
+        credentials = await super(JWTBearer, self).__call__(request)
+        if credentials:
+            if not credentials.scheme == "Bearer":
+                raise HTTPException(
+                    status_code=403, detail="Invalid authendication scheme"
+                )
+            if not self.verify_jwt(credentials.credentials):
+                raise HTTPException(status_code=403, detail="Invalid token or expired token")
+            return credentials.credentials
+        else:
+            raise HTTPException(status_code=403, detail="Invalid authorization code")
+
+
+    def verify_jwt(self, jwt_token: str) -> bool:
+        is_token_valid: bool = False
+
+        try:
+            payload = jwt.decode(jwt_token, SECRET_KEY, algorithms=ALGORITHM)
+        except:
+            payload = None
+
+        if payload:
+            is_token_valid = True
+        return is_token_valid
+
+
+def get_token_data(token: str = Depends(JWTBearer())):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    token_data = JWTRepo.decode_token(token)
+    if token_data is None:
+        raise credentials_exception
+    return token_data
+
+     
+def access_only_admin(
+    token_data: dict = Depends(get_token_data), db=Depends(get_db)
+):
+    user_email = token_data["sub"]
+    existing_user = UsersRepository.get_by_email(db=db, email=user_email)
+
+    if not existing_user:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="User not found",
+        )
+
+    if existing_user.role_id != 2:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="You are not a admin",
+        )
+
+    return existing_user
+
+
+def access_only_standard_user(
+    token_data: dict = Depends(get_token_data), db=Depends(get_db)
+):
+    user_email = token_data["sub"]
+    existing_user = UsersRepository.get_by_email(db=db, email=user_email)
+
+    if not existing_user:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="User not found",
+        )
+
+    if existing_user.role_id != 1:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="You are not a standard user",
+        )
+
+    return existing_user
+
+
+def access_only_researcher(
+    token_data: dict = Depends(get_token_data), db=Depends(get_db)
+):
+    user_email = token_data["sub"]
+    existing_user = UsersRepository.get_by_email(db=db, email=user_email)
+
+    if not existing_user:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="User not found",
+        )
+
+    if existing_user.role_id != 3:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="You are not a researcher",
+        )
+
+    return existing_user
+
+
+def only_authorized_user(
+    token_data: dict = Depends(get_token_data), db=Depends(get_db)
+):
+    user_email = token_data["sub"]
+    existing_user = get_user_by_email(db=db, email=user_email)
+    if not existing_user:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="User not found",
+        )
+    return existing_user
