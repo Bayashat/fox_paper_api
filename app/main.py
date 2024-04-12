39cbfec2f353241c03546f286c4d9ecc8a8d6e0a
Bayashat <baiashat2002@gmail.com>
2024-04-12 21:58:25 +0500

    add: routes of users, researches, comments

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
-from .src.routers.views import auth, researches
+from app.src.routers.views import researches, users, auth, comments
 
 app = FastAPI()
 
 app.include_router(auth.router, prefix="/auth", tags=["auth"])
+app.include_router(users.router, prefix="/users", tags=["users"])
 app.include_router(researches.router, prefix="/researches", tags=["researches"])
+app.include_router(comments.router, prefix="/researches", tags=["comments"])
+
