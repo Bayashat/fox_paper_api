5d7adb5de2b99bd70d64f3a8215835f1cb3bb25d
Bayashat <baiashat2002@gmail.com>
2024-04-12 15:11:16 +0500

    fix: minor bugs

--- app/src/routers/views/auth.py
+++ app/src/routers/views/auth.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException, status, Response
 from sqlalchemy.orm import Session
-from passlib.context import CryptContext
 from app.src.dependencies import (
     get_db,
     only_authorized_user,
@@ -11,41 +10,24 @@ from app.src.dependencies import (
 from app.src.routers.repositories.users import UsersRepository
 from ..schemas.users import SignupSchema, LoginSchema, ProfileSchema, UserModel
 from app.src.dependencies import JWTRepo
-
+from ..services.users import user_signup_validate, user_login_validate
 
 router = APIRouter()
-
-# Encrypt password
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 @router.post("/signup", status_code=status.HTTP_201_CREATED)
 def signup(user: SignupSchema, db: Session = Depends(get_db)):
-    existing_user = UsersRepository.get_by_email(db, user.email)
-    if existing_user:
-        raise HTTPException(
-            status_code=400, detail="User by that email already registered"
-        )
-
-    if UsersRepository.get_by_phone(db, user.phone_number):
-        raise HTTPException(
-            status_code=400, detail="User by that phone number already registered"
-        )
-    UsersRepository.create_user(db, user)
-    return {"message": user}
+    if user_signup_validate(db=db, user=user):
+        UsersRepository.create_user(db, user)
+        return Response(content="User created", status_code=status.HTTP_201_CREATED)
 
 
 @router.post("/login", status_code=status.HTTP_200_OK)
 def user_login(request: LoginSchema, db: Session = Depends(get_db)):
-    existing_user = UsersRepository.get_by_email(db, request.email)
-    if existing_user is None:
-        raise HTTPException(status_code=400, detail="User not found")
-
-    if not pwd_context.verify(request.password, existing_user.password):
-        raise HTTPException(status_code=400, detail="Incorrect password")
-
-    token = JWTRepo.generate_token({"sub": request.email})
-    return {"access_token": token, "token_type": "Bearer"}
+    if user_login_validate(db=db, user=request):
+        token = JWTRepo.generate_token({"sub": request.email})
+        return {"access_token": token, "token_type": "Bearer"}
+    
 
 
 @router.get("/profile", response_model=ProfileSchema, status_code=status.HTTP_200_OK)
