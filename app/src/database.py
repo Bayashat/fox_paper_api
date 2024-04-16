c02d78a22a2d57fd35f0f7d1c25bf9c6086fcd2f
Bayashat <baiashat2002@gmail.com>
2024-04-16 14:15:57 +0500

    changed db to sqlite & deploy fix

--- app/src/database.py
+++ app/src/database.py
@@ -5,7 +5,10 @@ from sqlalchemy.orm import sessionmaker
 Base = declarative_base()
 
 engine = create_engine(
-    url = "postgresql://admin:admin@172.22.0.2:5432/fox_paper"
+    # url = "postgresql://admin:admin@localhost:5432/fox_paper"
+    # url = "postgresql://admin:admin@172.22.0.2:5432/fox_paper"
+    url = "sqlite:///./sql_app.db",
+    connect_args={"check_same_thread": False}
 )
 
 
